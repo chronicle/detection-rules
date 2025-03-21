8858a5c9f090ea34a48b47df8a63dbe28674f725
David French <frenchee@google.com>
2025-03-21 07:32:55 -0700

    minor updates to logging, retries, and docstrings
    PiperOrigin-RevId: 739174186

--- tools/rule_manager/google_secops_api/rules/list_rule_deployments.py
+++ tools/rule_manager/google_secops_api/rules/list_rule_deployments.py
@@ -18,11 +18,14 @@ API reference:
 https://cloud.google.com/chronicle/docs/reference/rest/v1alpha/projects.locations.instances.rules.deployments/list
 """
 
+import logging
 import os
 import time
 from typing import Any, List, Mapping, Tuple
 
 from google.auth.transport import requests
+
+LOGGER = logging.getLogger()
 
 
 def list_rule_deployments(
@@ -34,39 +37,42 @@ def list_rule_deployments(
   """Retrieve a list of rule deployment states.
 
   Args:
-      http_session: Authorized session for HTTP requests.
-      page_size (optional): Maximum number of rule deployment states to return.
-        Must be non-negative, and is capped at a server-side limit of 1000. A
-        server-side default of 100 is used if the size is 0 or a None value.
-      page_token (optional): Page token from a previous ListRuleDeployments call
-        used for pagination. The first page is retrieved if the token is the
-        empty string or a None value.
-      max_retries (optional): Maximum number of times to retry HTTP request if
-        certain response codes are returned. For example: HTTP response status
-        code 429 (Too Many Requests)
+    http_session: Authorized session for HTTP requests.
+    page_size (optional): Maximum number of rule deployment states to return.
+      Must be non-negative, and is capped at a server-side limit of 1000. A
+      server-side default of 100 is used if the size is 0 or a None value.
+    page_token (optional): Page token from a previous ListRuleDeployments call
+      used for pagination. The first page is retrieved if the token is the
+      empty string or a None value.
+    max_retries (optional): Maximum number of times to retry HTTP request if
+      certain response codes are returned. For example: HTTP response status
+      code 429 (Too Many Requests)
 
   Returns:
-      List of rule deployments and a page token for the next page of rule
-      deployments, if there are any.
-          Reference:
-          https://cloud.google.com/chronicle/docs/reference/rest/v1alpha/RuleDeployment
+    List of rule deployments and a page token for the next page of rule
+      deployments, if there are any. Reference:
+      https://cloud.google.com/chronicle/docs/reference/rest/v1alpha/RuleDeployment
 
   Raises:
-      requests.exceptions.HTTPError: HTTP request resulted in an error
-      (response.status_code >= 400).
+    requests.exceptions.HTTPError: HTTP request resulted in an error
+    (response.status_code >= 400).
+    requests.exceptions.JSONDecodeError: If the server response is not valid
+    JSON.
   """
   url = f"{os.environ['GOOGLE_SECOPS_API_BASE_URL']}/{os.environ['GOOGLE_SECOPS_INSTANCE']}/rules/-/deployments"
   params = {"page_size": page_size, "page_token": page_token}
   response = None
 
-  for _ in range(max_retries + 1):
+  for _ in range(max(max_retries, 0) + 1):
     response = http_session.request(method="GET", url=url, params=params)
 
     if response.status_code >= 400:
-      print(response.text)
+      LOGGER.warning(response.text)
 
     if response.status_code == 429:
-      print("API rate limit exceeded. Sleeping for 60s before retrying")
+      LOGGER.warning(
+          "API rate limit exceeded. Sleeping for 60s before retrying"
+          )
       time.sleep(60)
     else:
       break
